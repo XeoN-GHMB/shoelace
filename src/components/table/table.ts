3c4b0ac68e445f446df66c354d587c22f5e8b393
Andreas H. Kelch <ak@mausbrand.de>
2022-05-13 06:06:56 +0200

    some more table features

--- src/components/table/table.ts
+++ src/components/table/table.ts
@@ -4,7 +4,7 @@ import { emit } from '../../internal/event';
 import { watch } from '../../internal/watch';
 import { watchProps } from '../../internal/watchProps';
 import styles from './table.styles';
-import {TabulatorFull} from './tabulator_esm.js';
+import {TabulatorFull, RowComponent, FormatModule} from './tabulator_esm.js';
 
 /**
  * @since 2.0
@@ -30,21 +30,25 @@ export default class SlTable extends LitElement {
 
   @property({type:String, reflect: true}) placeholder:String = "No Data Available"
   @property({type:String, reflect: true}) height:String = "auto"
+  @property({type:String, reflect: true}) minheight:String = "auto"
   @property({type:String, reflect: true}) sort:any = null
   @property({type:String, reflect: true}) search:String = ''
+
   @property({ type: Boolean, reflect: true }) moveablecolumns:Boolean = false;
+  @property({ type: Boolean, reflect: true }) moveablerows:Boolean = false;
 
   @property({ type: Boolean, reflect: true }) rowindexes:Boolean = false;
-  @property({ type: Boolean, reflect: true }) moveablerows:Boolean = false;
   @property({ type: Boolean, reflect: true }) rowselect:Boolean = false;
-  @property({ type: Boolean, reflect: true }) columnsmenu:Boolean = false;
+
+  @property({ type: Boolean, reflect: true }) nocolumnsmenu:Boolean = false;
 
   @property({ type: Array, attribute: false }) skellist: Object;
   @property({ type: Object, attribute: false }) structure: Object;
   @property({ type: Object, attribute: false }) tableConfig: Object = {
       responsiveLayout:"hide",
       layout:"fitColumns",
-      reactiveData:true
+      reactiveData:true,
+      popupContainer:true
     };
 
   tableInstance:any;
@@ -60,7 +64,9 @@ export default class SlTable extends LitElement {
         this.buildStructure()
         this.updateConfig()
         console.log(this.tableConfig)
-
+        if (!this.shadowtable){
+          return 0
+        }
         this.tableInstance = new TabulatorFull(this.shadowtable, this.tableConfig)
         this.tableInstance.on("tableBuilt",()=>{
             this.postBuildTable()
@@ -117,7 +123,7 @@ export default class SlTable extends LitElement {
       if(Object.keys(item).includes("visible") && !item["visible"]){
         continue
       }
-      columns.push({title:item["descr"], field:itemName})
+      columns.push({title:item["descr"], field:itemName, formatterParams:item})
     }
 
     currentstructure["columns"] = columns
@@ -126,6 +132,7 @@ export default class SlTable extends LitElement {
 
   updateConfig(){
     this.tableConfig["height"] = this.height
+    this.tableConfig["minHeight"] = this.minheight
     this.tableConfig["placeholder"] = this.placeholder
     if (this.sort){
        this.tableConfig["initialSort"] = [{column:this.sort, dir:"asc"}]
@@ -145,7 +152,7 @@ export default class SlTable extends LitElement {
     }
 
     if (this.rowselect){
-      let selectColumn = {formatter:"rowSelection", resizable:false, width:45, minWidth:45, titleFormatter:"rowSelection", hozAlign:"center", headerSort:false, cellClick:function(e, cell){
+      let selectColumn = {formatter:this.slRowSelection, resizable:false, width:47, minWidth:47, titleFormatter:this.slRowSelection, hozAlign:"center", headerSort:false, cellClick:function(e, cell){
         cell.getRow().toggleSelect();
       }}
 
@@ -158,7 +165,7 @@ export default class SlTable extends LitElement {
     }
 
     if(this.moveablerows){
-      this.tableConfig["movableRows"] = true 
+      this.tableConfig["movableRows"] = true
 
       let handleColumn = {rowHandle:true, resizable:false, formatter:"handle", headerSort:false, frozen:true, width:40, minWidth:40}
 
@@ -169,33 +176,161 @@ export default class SlTable extends LitElement {
       }
     }
 
-    if (this.columnsmenu){
-        this.tableConfig["rowContextMenu"] = this.rowMenu
-
-        let menuColumn = {resizable:false, headerSort:false, frozen:true, width:4, minWidth:40, headerMenu:this.headerMenu}
-
+    if (!this.nocolumnsmenu){
         if ( !Object.keys(this.tableConfig).includes("columns") || this.tableConfig["columns"].length===0){
+          let menuColumn = {resizable:false, headerSort:false, frozen:true, width:47, minWidth:47, headerMenu:this.headerMenu}
           this.tableConfig["columns"] = [menuColumn]
         }else{
-          this.tableConfig["columns"] = [ menuColumn ,...this.tableConfig["columns"]]
+          for(let col of this.tableConfig["columns"] ){
+            if (["handle","rownum",this.slRowSelection].includes(col["formatter"])){
+              continue
+            }
+            col["headerMenu"]=this.headerMenu
+          }
+          //this.tableConfig["columns"] = [ menuColumn ,...this.tableConfig["columns"]]
         }
     }
+
+    for(let col of this.tableConfig["columns"] ){
+      //col["formatter"] = this.vCellRenderer
+    }
+
   }
 
   headerMenu(){
-    return [{
-            label:"<span>hallo</span>",
-            action:(e)=>{console.log("HALLO")}
-    }]
+    var menu = [];
+    var columns = this.getColumns(); //this == TabulatorInstance
+
+    for(let column of columns){
+        let titleString = column.getDefinition().title
+        if (!titleString){
+          continue
+        }
+
+        let label = document.createElement("span");
+        let title = document.createElement("span");
+        title.textContent = " " + titleString;
+        let check = document.createElement("sl-checkbox")
+
+        label.appendChild(check);
+        label.appendChild(title);
+
+        if(column.isVisible()){
+            check.checked = true
+        }else{
+            check.checked = false
+        }
+
+        //create menu item
+        menu.push({
+            label:label,
+            action:function(event){
+                event.stopPropagation();
+                event.preventDefault();
+
+                column.toggle();
+                  if(column.isVisible()){
+                      check.checked = true
+                  }else{
+                      check.checked = false
+                  }
+            }
+        });
+    }
+   return menu;
+
   }
 
-  rowMenu(){
-    return [{
-        label:"<span>hallo</span>",
-        action:function(e, row){
-            row.update({name:"Steve Bobberson"});
+  slRowSelection(cell, formatterParams, onRendered){
+    var checkbox = document.createElement("sl-checkbox");
+    var blocked = false;
+
+    checkbox.setAttribute("aria-label", "Select Row");
+
+    if(this.table.modExists("selectRow", true)){
+
+      checkbox.addEventListener("click", (e) => {
+        e.stopPropagation();
+      });
+
+      if(typeof cell.getRow == 'function'){
+        var row = cell.getRow();
+
+        if(row instanceof RowComponent){
+
+          checkbox.addEventListener("sl-change", (e) => {
+            if(this.table.options.selectableRangeMode === "click"){
+              if(!blocked){
+                row.toggleSelect();
+              }else {
+                blocked = false;
+              }
+            }else {
+              row.toggleSelect();
+            }
+          });
+
+          if(this.table.options.selectableRangeMode === "click"){
+            checkbox.addEventListener("click", (e) => {
+              blocked = true;
+              this.table.modules.selectRow.handleComplexRowClick(row._row, e);
+            });
+          }
+
+          checkbox.checked = row.isSelected && row.isSelected();
+          this.table.modules.selectRow.registerRowSelectCheckbox(row, checkbox);
+        }else {
+          checkbox = "";
         }
-    }]
+      }else {
+        checkbox.addEventListener("sl-change", (e) => {
+          if(this.table.modules.selectRow.selectedRows.length){
+            this.table.deselectRow();
+          }else {
+            this.table.selectRow(formatterParams.rowRange);
+          }
+        });
+
+        this.table.modules.selectRow.registerHeaderSelectCheckbox(checkbox);
+      }
+    }
+
+    return checkbox;
+  }
+
+  vCellRenderer(cell, formatterParams, onRendered){
+    /* Defaults
+    plaintext, textarea, html, money, image, link, datetime,
+    datetimediff, tickCross, color, star, traffic, progress,
+    lookup, buttonTick, buttonCross, rownum, handle,
+
+    select = lookup
+    file = image, link
+    plain = string, numeric
+    text = textare, html
+    date = datetime
+    color = color
+
+    http://tabulator.info/docs/5.2/format#format-custom
+
+    lang > multi > types
+
+    * */
+
+    let languageWrapper = ()=>{
+
+    }
+
+    let multipleWrapper = ()=>{
+
+    }
+    console.log(this.table)
+    console.log(this)
+
+
+    let formatMod = new FormatModule(this.tableInstance)
+    return formatMod.formatters.plaintext(cell,formatterParams,onRendered)
+
   }
 
 
@@ -222,20 +357,20 @@ export default class SlTable extends LitElement {
     })
   }
 
-
   prebuildTable(){
     let tableElement = this.getTable()
     this.updateConfig()
     console.log(this.tableConfig)
+    if (!tableElement){
+          return 0
+        }
     if(tableElement){
       this.tableInstance = new TabulatorFull(tableElement,
       this.tableConfig)
 
       this.postBuildTable()
     }
-
   }
-
 
   slotContent(){
      const childs = this.shadowRoot!.querySelector("slot").assignedElements({flatten: true})
@@ -261,4 +396,6 @@ declare global {
     'sl-table': SlTable;
   }
 }
+
+
 
