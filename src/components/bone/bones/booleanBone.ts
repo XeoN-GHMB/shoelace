c82cddef5d7597b9dc140bdc0d8469339078a8fc
Arne Gudermann <ag@mausbrand.de>
2022-11-29 08:28:31 +0100

    Add EventListener to boolean bone

--- src/components/bone/bones/booleanBone.ts
+++ src/components/bone/bones/booleanBone.ts
@@ -7,8 +7,13 @@ export class BooleanBone extends RawBone {
 
   getEditor(value: boolean, boneName: string, lang: string | null = null): HTMLElement {
     const inputElement: SlSwitch = document.createElement("sl-switch");
+
     inputElement.dataset.boneName = boneName;
     inputElement.name = boneName;
+    inputElement.addEventListener("sl-change", () => {
+      this.mainInstance.internboneValue = this.reWriteBoneValue();
+      this.mainInstance.handleChange();
+    });
     if (value !== null && value !== undefined) {
       inputElement.checked = value;
     }
