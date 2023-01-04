e9e6dc3abd628a0d258a1722d6675721ea123f31
Arne Gudermann <ag@mausbrand.de>
2023-01-04 17:17:02 +0100

    Calc sortindex new and better row movemend

--- src/components/table/modules/tabulator_move_rows.ts
+++ src/components/table/modules/tabulator_move_rows.ts
@@ -1,100 +1,259 @@
-import {Tabulator, MoveRowsModule, Helpers, RowComponent} from '../tabulator_esm.js';
+import {Tabulator, MoveRowsModule, RowComponent, Row} from '../tabulator_esm.js';
+
 
 class CustomMoveRowsModule extends MoveRowsModule {
   constructor(table: Tabulator) {
     super(table);
     this.tableInstance = table;
-    this.hoverRow = false
+    this.hoverRow = false;
+    this.addAsChild = false;
+  }
+
+  startMove(e, row) {
+    console.log("start mobve ? ")
+    super.startMove(e, row)
+    this.hoverElement.style.height = "50px";
+    this.hoverElement.style.opacity = "0.5";
+    //row.getComponent().treeCollapse();
   }
 
   initializeRow(row: RowComponent) {
+    super.initializeRow(row)
     if (!this.tableInstance.options["dataTree"])  //If we not have a datatree we only call the main function
     {
-      super.initializeRow(row)
+
       return
     }
-
-
-    super.initializeRow(row)
     var self = this,
       config = {},
       rowEl;
 
-    //inter table drag drop
-    config.mouseup = function (e) {
-      self.tableRowDrop(e, row);
-    }.bind(self);
-
     //same table drag drop
     config.mousemove = function (e) {
-      var rowEl = row.getElement();
-      this.hoverRow = row;
-    }.bind(self);
-
-
-    if (!this.hasHandle) {
 
       rowEl = row.getElement();
 
-      rowEl.addEventListener("mousedown", function (e) {
-        if (e.which === 1) {
-          self.checkTimeout = setTimeout(function () {
-            self.startMove(e, row);
-          }, self.checkPeriod);
+      if (row !== self.hoverRow) {
+
+        //row.getComponent().select();
+        if (self.hoverRow) {
+          self.hoverRow.getComponent().deselect();
         }
-      });
 
-      rowEl.addEventListener("mouseup", function (e) {
-        if (e.which === 1) {
-          if (self.checkTimeout) {
-            clearTimeout(self.checkTimeout);
-          }
+      }
+
+      self.hoverRow = row;
+      if (((e.pageY - Helpers.elOffset(rowEl).top) + self.table.rowManager.element.scrollTop) > (row.getHeight() / 2)) {
+        if (self.toRow !== row || !self.toRowAfter) {
+          rowEl.parentNode.insertBefore(self.placeholderElement, rowEl.nextSibling);
+          self.moveRow(row, true);
         }
-      });
-
-      this.bindTouchEvents(row, row.getElement());
-    }
-
+      } else {
+        if (self.toRow !== row || self.toRowAfter) {
+          rowEl.parentNode.insertBefore(self.placeholderElement, rowEl);
+          self.moveRow(row, false);
+        }
+      }
+    }.bind(self);
     row.modules.moveRow = config;
   }
 
+  createPlaceholderElement() {
+    var el = document.createElement("div");
+    el.classList.add("tabulator-col");
+    el.classList.add("tabulator-col-placeholder");
+    el.style.backgroundColor = "blue"
+    el.style.height = "10px"
+
+    return el;
+  }
+
+  moveHover(e) {
+    super.moveHover(e);
+    if (!this.hoverRow) {
+      return
+    }
+    const rowHeight = this.hoverRow.getElement().clientHeight;
+    const inRowY = (e.pageY - Helpers.elOffset(this.hoverRow.getElement()).top);
+    const minYChild = rowHeight / 5;
+    const maxYChild = rowHeight / 5 * 4;
+
+    if (inRowY > minYChild && inRowY < maxYChild) {
+      this.addAsChild = true;
+      this.hoverRow.getComponent().select();
+      this.placeholderElement.style.height = "0px";
+    } else {
+      this.addAsChild = false;
+      this.placeholderElement.style.borderColor = "blue"
+      this.placeholderElement.style.marginLeft = "0px";
+      this.placeholderElement.style.height = "5px";
+      this.placeholderElement.innerText = "";
+      this.hoverRow.getComponent().deselect();
+    }
+    //if(inRowY )
+    //((e.pageY - Helpers.elOffset(rowEl).top) + self.table.rowManager.element.scrollTop) > (row.getHeight() / 2)
+
+  }
+
+
   endMove(e) {
+
+    console.log("try to end moving")
     if (!this.tableInstance.options["dataTree"])  //If we not have a datatree we only call the main function
     {
       super.endMove(e)
       return
     }
 
-    if (!e || e.which === 1 || this.touchMove) {
-      this._unbindMouseMove();
 
-      if (!this.connection) {
-        this.placeholderElement.parentNode.insertBefore(this.moving.getElement(), this.placeholderElement.nextSibling);
-        this.placeholderElement.parentNode.removeChild(this.placeholderElement);
+    if (this.hoverRow) {
+      this.hoverRow.getElement().style.borderWidth = "0px";
+      const self = this;
+      const hoverRowComponent: RowComponent = this.hoverRow.getComponent();
+      const movingRowComponent: RowComponent = this.moving.getComponent();
+      if (this.addAsChild) {
 
-      }
-      if (this.hoverRow) {
-        this.hoverElement.parentNode.removeChild(this.hoverElement);
-        this.table.element.classList.remove("tabulator-block-select");
-
-        const hoverRowComponent: RowComponent = this.hoverRow.getComponent();
-        const movingRowComponent: RowComponent = this.moving.getComponent();
         if (hoverRowComponent._row.data["_children"][0] === undefined) {
           hoverRowComponent._row.data["_children"] = [];
         }
-        hoverRowComponent.addTreeChild(this.moving.getData())
+        console.log(`update parent entry form ${movingRowComponent.getData()["parententry"]} to ${hoverRowComponent.getData()["key"]}`)
+        const tmp_data = movingRowComponent.getData();
+        tmp_data["parententry"] = hoverRowComponent.getData()["key"];
+        movingRowComponent.update(tmp_data).then(() => {
+          console.log(`parent entry form ${movingRowComponent.getData()["parententry"]}`)
+          this.moveRowToChild(hoverRowComponent, movingRowComponent)
+
+          self.moving.table.rowManager.deleteRow(self.moving);
+          self.dispatchExternal("rowMovedDataTree", {
+            "move": true,
+            "sortindex": false,
+            "after":this.toRowAfter,
+            "srcRow": movingRowComponent,
+            "destRow": hoverRowComponent
+          });
+          self._endMove(e);
+        });
+
+
+      } else { //change sort index and move maybe
+        console.log("Move sort index")
+        console.log("moving name=", movingRowComponent.getData()["name"], movingRowComponent.getData())
+        console.log("hover name=", hoverRowComponent.getData()["name"], hoverRowComponent.getData())
+        console.log("same parent=", movingRowComponent.getData()["parententry"] === hoverRowComponent.getData()["parententry"])
+        console.log("after =", this.toRowAfter)
+
 
         if (movingRowComponent.getTreeParent()) {
 
-          const idx = movingRowComponent.getTreeParent().getData()["_children"].indexOf(this.moving.getData());
 
-          movingRowComponent.getTreeParent().removeTreeChild(idx)
-          console.log("data=>", movingRowComponent.getTreeParent().getData()["_children"])
+          if (movingRowComponent.getData()["parententry"] === hoverRowComponent.getData()["parententry"]) {
+            console.log("We change only the sort index");
+            this.dispatchExternal("rowMovedDataTree", {
+              "move": false,
+              "sortindex": true,
+              "after": this.toRowAfter,
+              "srcRow": movingRowComponent,
+              "destRow": hoverRowComponent
+            });
+            this.table.rowManager.moveRow(this.moving, this.toRow, this.toRowAfter);
+            this.placeholderElement.parentNode.insertBefore(this.moving.getElement(), this.placeholderElement.nextSibling);
+            this.placeholderElement.parentNode.removeChild(this.placeholderElement);
+            this._endMove()
+          } else // we must move and set new sort index
+          {
+            this.dispatchExternal("rowMovedDataTree", {
+              "move": true,
+              "sortindex": true,
+              "after":this.toRowAfter,
+              "srcRow": movingRowComponent,
+              "destRow": hoverRowComponent
+            });
+            //we must move
+            console.log("we must move")
+            if (hoverRowComponent.getTreeParent()) {//move form child to child
+
+            } else {
+              //move form child to root level
+              /*console.log("move form child to root level")
+              console.log("placeholderElement",this.placeholderElement.parentNode)
+              this.table.rowManager.moveRow(this.moving, this.toRow, this.toRowAfter);
+              this.placeholderElement.parentNode.insertBefore(this.moving.getElement(), this.placeholderElement.nextSibling);
+              this.placeholderElement.parentNode.removeChild(this.placeholderElement);*/
+              const tmp_data = movingRowComponent.getData();
+              tmp_data["parententry"] = hoverRowComponent.getData()["parententry"];
+              movingRowComponent.update(tmp_data).then(() => {
+                self.moveRowToRoot(hoverRowComponent, movingRowComponent, self.toRowAfter)
+                self.moving.table.rowManager.deleteRow(self.moving);
+                this._endMove(e);
+              })
+            }
+
+
+          }
+        } else {
+          if (hoverRowComponent.getTreeParent()) {
+
+
+            if (movingRowComponent.getData()["parententry"] === hoverRowComponent.getData()["parententry"]) {
+              this.table.rowManager.moveRow(this.moving, this.toRow, this.toRowAfter);
+              this.placeholderElement.parentNode.insertBefore(this.moving.getElement(), this.placeholderElement.nextSibling);
+              this.placeholderElement.parentNode.removeChild(this.placeholderElement);
+              this._endMove(e);
+
+            } else {
+              const hoverRowComponentParent = hoverRowComponent.getTreeParent();
+              this.table.rowManager.moveRow(this.moving, this.toRow, this.toRowAfter);
+              this.placeholderElement.parentNode.insertBefore(this.moving.getElement(), this.placeholderElement.nextSibling);
+              this.placeholderElement.parentNode.removeChild(this.placeholderElement);
+              const tmp_data = movingRowComponent.getData();
+              tmp_data["parententry"] = hoverRowComponentParent.getData()["key"];
+              movingRowComponent.update(tmp_data).then(() => {
+                self.moveRowToChild(hoverRowComponentParent, movingRowComponent, hoverRowComponent, self.toRowAfter)
+                self.moving.table.rowManager.deleteRow(self.moving);
+                this._endMove(e)
+              })
+
+
+            }
+
+
+          } else {
+            this.table.rowManager.moveRow(this.moving, this.toRow, this.toRowAfter);
+
+            this.placeholderElement.parentNode.insertBefore(this.moving.getElement(), this.placeholderElement.nextSibling);
+            this.placeholderElement.parentNode.removeChild(this.placeholderElement);
+
+            this.dispatchExternal("rowMovedDataTree", {
+              "move": false,
+              "sortindex": true,
+              "after": this.toRowAfter,
+              "srcRow": movingRowComponent,
+              "destRow": hoverRowComponent
+            });
+            this._endMove(e)
+          }
+
+
         }
-        this.moving.table.rowManager.deleteRow(this.moving);
-        this.dispatchExternal("rowMovedDataTree", {"srcRow": movingRowComponent, "destRow": hoverRowComponent});
+
 
       }
+
+    }
+
+  }
+
+  _endMove(e) {
+    if (!e || e.which === 1 || this.touchMove) {
+      this._unbindMouseMove();
+
+      try {
+        this.hoverElement.parentNode.removeChild(this.hoverElement);
+      } catch (err) {
+        console.error(err)
+      }
+
+      this.table.element.classList.remove("tabulator-block-select");
 
 
       this.moving = false;
@@ -106,8 +265,119 @@ class CustomMoveRowsModule extends MoveRowsModule {
 
 
     }
+
+  }
+
+  moveRowToChild(destRowComponent, srcRowComponent, childComponent = null, after = true) {
+
+    if (destRowComponent._row.data["_children"][0] === undefined) {
+      destRowComponent._row.data["_children"] = [];
+    }
+    let idxChild = null;
+    let idx = -1;
+    if (childComponent) {
+      destRowComponent.getTreeChildren().forEach((child, i) => {
+        if (childComponent === child) {
+
+          idxChild = child;
+          idx = i;
+          return;
+        }
+      })
+    }
+    if (idxChild !== null) {
+      console.log(idx === 0 && !after, idxChild)
+      if (!after && idx > 0) {
+        idxChild = destRowComponent.getTreeChildren()[idx - 1];
+      }
+      destRowComponent.addTreeChild(srcRowComponent.getData(), idx === 0 && !after, idxChild)
+    } else {
+      destRowComponent.addTreeChild(srcRowComponent.getData())
+    }
+
+
+    if (srcRowComponent.getTreeParent()) {
+      const idx = srcRowComponent.getTreeParent().getData()["_children"].indexOf(srcRowComponent.getData());
+      srcRowComponent.getTreeParent().removeTreeChild(idx)
+
+    }
+  }
+
+  moveRowToRoot(destRowComponent, srcRowComponent, after = true) {
+    if (srcRowComponent.getTreeParent()) {
+      const idx = srcRowComponent.getTreeParent().getData()["_children"].indexOf(srcRowComponent.getData());
+      srcRowComponent.getTreeParent().removeTreeChild(idx)
+    }
+    console.log("after", after )
+    console.log("destrow", destRowComponent)
+
+    //WTF  the docu is completely wrong here (addData)
+    if (!after && destRowComponent.getPosition() ===1) {
+      this.tableInstance.addData(srcRowComponent.getData(),true);
+    } else {
+      if(after)
+      {
+        this.tableInstance.addData(srcRowComponent.getData(), undefined, destRowComponent);
+      }
+      else
+      {
+        this.tableInstance.addData(srcRowComponent.getData(), undefined, destRowComponent.getPrevRow());
+      }
+
+    }
+
   }
 }
 
 CustomMoveRowsModule.moduleName = "moveRow";
 export {CustomMoveRowsModule};
+
+///Hard copy helper class
+class Helpers {
+
+  static elVisible(el) {
+    return !(el.offsetWidth <= 0 && el.offsetHeight <= 0);
+  }
+
+  static elOffset(el) {
+    var box = el.getBoundingClientRect();
+
+    return {
+      top: box.top + window.pageYOffset - document.documentElement.clientTop,
+      left: box.left + window.pageXOffset - document.documentElement.clientLeft
+    };
+  }
+
+  static deepClone(obj, clone, list = []) {
+    var objectProto = {}.__proto__,
+      arrayProto = [].__proto__;
+
+    if (!clone) {
+      clone = Object.assign(Array.isArray(obj) ? [] : {}, obj);
+    }
+
+    for (var i in obj) {
+      let subject = obj[i],
+        match, copy;
+
+      if (subject != null && typeof subject === "object" && (subject.__proto__ === objectProto || subject.__proto__ === arrayProto)) {
+        match = list.findIndex((item) => {
+          return item.subject === subject;
+        });
+
+        if (match > -1) {
+          clone[i] = list[match].copy;
+        } else {
+          copy = Object.assign(Array.isArray(subject) ? [] : {}, subject);
+
+          list.unshift({subject, copy});
+
+          clone[i] = this.deepClone(subject, copy, list);
+        }
+      }
+    }
+
+    return clone;
+  }
+}
+
--- src/components/table/table.ts
+++ src/components/table/table.ts
@@ -106,7 +106,6 @@ export default class SlTable extends ShoelaceElement {
 
   @watchProps(['structure', 'skellist', "editabletable", "nodes", "mode"])
   optionUpdate() {
-    console.log("update", this.nodes)
     //only rebuild table if structure changed
     if (this.mode === "list") {
       if (this.skellist === undefined || this.structure === undefined || Object.keys(this.structure).length === 0) {
@@ -171,10 +170,35 @@ export default class SlTable extends ShoelaceElement {
         if (this.mode === "hierarchy") {
           const self = this;
           this.tableInstance.on("rowMovedDataTree", (detail) => {
+            console.log(`row moved move:${detail.move} sort index ${detail.sortindex} after:${detail.after}`)
             const eventdata = {};
-            eventdata["srcKey"] = detail["srcRow"].getData()["key"]
-            eventdata["destKey"] = detail["destRow"].getData()["key"]
-            self.emit("table-rowMovedDataTree", {detail: eventdata});
+
+
+            if (detail.move && !detail.sortindex) {
+
+              eventdata["srcKey"] = detail["srcRow"].getData()["key"];
+              eventdata["destKey"] = detail["destRow"].getData()["key"];
+              self.emit("table-rowMovedDataTree", {detail: eventdata});
+
+            } else if (!detail.move && detail.sortindex) {
+
+
+              console.log("des row", detail["destRow"].getElement())
+              const newSortIndex = this.calcSortIndex(detail)
+              eventdata["srcKey"] = detail["srcRow"].getData()["key"];
+              eventdata["sortindex"] = newSortIndex
+              console.log("new sort index", eventdata)
+              self.emit("table-newSortIndex", {detail: eventdata});
+
+
+            } else if (detail.move && detail.sortindex) {
+              const newSortIndex = this.calcSortIndex(detail)
+              eventdata["srcKey"] = detail["srcRow"].getData()["key"];
+              eventdata["destKey"] = detail["destRow"].getData()["key"];
+              eventdata["sortindex"] = newSortIndex
+              self.emit("table-move_and_SortIndex", {detail: eventdata});
+            }
+
 
           });
 
@@ -183,7 +207,7 @@ export default class SlTable extends ShoelaceElement {
       }
       if (this.mode === "hierarchy") {
 
-        const self = this;//keep instance hack
+        const self = this;//keep the instance hack
         this.tableInstance.on("dataTreeRowExpanded", function (row, level: number) {//we must fetch new data
           self.expandDepth = Math.max(self.expandDepth, level);
 
@@ -216,6 +240,43 @@ export default class SlTable extends ShoelaceElement {
 
     }
     return 1
+  }
+
+  calcSortIndex(detail) {
+
+    let nextRow = detail["destRow"].getNextRow();
+    if (nextRow)// we have the src row
+    {
+      nextRow = nextRow.getNextRow();
+    }
+
+    const prevRow = detail["destRow"].getPrevRow();
+    const destRow = detail["destRow"];
+    let newSortIndex = -1;
+    console.log("dest idx", detail["destRow"].getData()["sortindex"])
+    console.log("prevRow idx", prevRow.getData()["sortindex"])
+    console.log("nextRow idx", nextRow.getData()["sortindex"])
+
+    console.log("src", detail["srcRow"].getElement())
+    if (detail["after"]) {
+      if (nextRow) {
+        newSortIndex = (nextRow.getData()["sortindex"] + destRow.getData()["sortindex"]) / 2.0
+      } else {
+        newSortIndex = destRow.getData()["sortindex"] - 1
+      }
+    } else {
+      if (prevRow) {
+        newSortIndex = (prevRow.getData()["sortindex"] + destRow.getData()["sortindex"]) / 2.0
+      } else {
+        newSortIndex = destRow.getData()["sortindex"] + 1
+      }
+
+    }
+    if (newSortIndex === -1) {
+      console.log("newSortIndex is negative")
+      return
+    }
+    return newSortIndex
   }
 
   addData(data) {
@@ -308,7 +369,6 @@ export default class SlTable extends ShoelaceElement {
 
     if (this.mode === "hierarchy") {
       this.tableConfig["dataTree"] = true;
-      this.tableConfig["dataTreeSelectPropagate"] = true;
     }
   }
 
@@ -430,7 +490,7 @@ export default class SlTable extends ShoelaceElement {
       }
 
       this.tableConfig["dataTreeExpandElement"] = '<sl-icon name="chevron-right"></sl-icon>';
-      this.tableConfig["dataTreeCollapseElement"] =  '<sl-icon name="chevron-down"></sl-icon>';
+      this.tableConfig["dataTreeCollapseElement"] = '<sl-icon name="chevron-down"></sl-icon>';
 
     }
   }
