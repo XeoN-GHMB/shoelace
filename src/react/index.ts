96fc4704f52c503d85505bdd4046d952c8a65cc5
Andreas H. Kelch <ak@mausbrand.de>
2023-01-20 09:17:14 +0100

    fix: checkbox, radio and switches now scale correct

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,10 +1,10 @@
 export { default as SlAcPanel } from './ac-panel';
-export { default as SlAlert } from './alert';
-export { default as SlAnimatedImage } from './animated-image';
 export { default as SlAnimation } from './animation';
-export { default as SlAvatar } from './avatar';
+export { default as SlAlert } from './alert';
 export { default as SlBackToTop } from './back-to-top';
+export { default as SlAvatar } from './avatar';
 export { default as SlBadge } from './badge';
+export { default as SlAnimatedImage } from './animated-image';
 export { default as SlBone } from './bone';
 export { default as SlBreadcrumb } from './breadcrumb';
 export { default as SlBreadcrumbItem } from './breadcrumb-item';
