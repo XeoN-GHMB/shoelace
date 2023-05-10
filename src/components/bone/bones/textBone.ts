add292486fb3cbae806153aa198f2a861094358b
Arne Gudermann <ag@mausbrand.de>
2023-05-10 17:14:14 +0200

    feat: Render Textarea if validHTML is none

--- src/components/bone/bones/textBone.ts
+++ src/components/bone/bones/textBone.ts
@@ -1,4 +1,5 @@
 // @ts-nocheck
+import type {AstNode, Editor} from "tinymce";
 /**
  * Todo/problem/fixme ??
  * the problem is tiny mce load the css from a server
@@ -13,12 +14,30 @@ import {createPath} from "../utils";
 import {FileBone} from "./fileBone";
 import {RawBone} from "./rawBone";
 import type {FileSkelValues} from "../interfaces";
-import type {AstNode, Editor} from "tinymce";
 
 import textBoneStyle from "../styles/textBone.styles";
 
 export class TextBone extends RawBone {
   getEditor(value: any, boneName: string, lang: string | null = null): HTMLElement {
+    //We render a textarea if we have no valid html
+    if (this.boneStructure["validHtml"] === null) {
+      const ele = document.createElement("textarea");
+      ele.dataset["name"] = boneName;
+      ele.dataset["textbone"] = "true";
+      ele.rows=15;
+      if(value!==null || value!==undefined)
+      {
+        ele.value=value;
+      }
+      ele.addEventListener('change', (e) => {
+        createPath(this.mainInstance.internboneValue, boneName, ele.value);
+        this.mainInstance.handleChange();
+      });
+
+      return ele;
+    }
+
+
     const ele = document.createElement("div");
     ele.dataset["name"] = boneName;
     ele.dataset["textbone"] = "true";
@@ -32,6 +51,16 @@ export class TextBone extends RawBone {
 
 
     const self = this;
+    let valid_classes = {};
+    let toolbar = "undo redo";
+    if (this.boneStructure["validHtml"] !== null) {
+      valid_classes = {"*": self.boneStructure["validHtml"]["validClasses"].join(" ")};
+      toolbar = 'undo redo | blocks | bold italic underline backcolor | '
+        + 'alignleft aligncenter alignright alignjustify | '
+        + 'table code'
+    }
+
+
     setTimeout(() => {
       tinymce.init({
         target: ele,
@@ -41,15 +70,11 @@ export class TextBone extends RawBone {
         skin: false,
         content_css: false,
         content_style: textBoneStyle.cssText,
-
-        toolbar: 'undo redo | blocks | bold italic underline backcolor | '
-          + 'alignleft aligncenter alignright alignjustify | '
-          + 'table code',
-
+        toolbar: toolbar,
         plugins: ["table", "code"],
         formats: self.getFormats(), // TODO add classes
         valid_elements: self.getValidTagString(),
-        valid_classes: {"*": self.boneStructure["validHtml"]["validClasses"].join(" ")},
+        valid_classes: valid_classes,
         readonly: self.boneStructure["readonly"],
         //images_upload_handler: self.uploadHandler,
 
@@ -66,14 +91,13 @@ export class TextBone extends RawBone {
             })
           })
           editor.on('init', () => {
-            if (value) {
-              editor.setContent(value);
-            }
+            editor.setContent(value);
             self.mainInstance.textboneCache[boneName] = editor;
           });
-          editor.on('Change', (e) => {
+          editor.on('change', (e) => {
+
             createPath(self.mainInstance.internboneValue, boneName, editor.getContent());
-            console.log("get content", editor.getContent())
+            console.log("get content", content)
             self.mainInstance.handleChange();
 
           });
@@ -151,11 +175,11 @@ export class TextBone extends RawBone {
   uploadHandler(blobInfo, progress) {
     //Todo error handling
     return new Promise((resolve, reject) => {
-      FileBone.getUploadUrl(blobInfo.blob(),this.mainInstance).then(uploadData => {
+      FileBone.getUploadUrl(blobInfo.blob(), this.mainInstance).then(uploadData => {
 
         FileBone.uploadFile(blobInfo.blob(), uploadData).then(resp => {
 
-          FileBone.addFile(uploadData,this.mainInstance).then((fileData: FileSkelValues) => {
+          FileBone.addFile(uploadData, this.mainInstance).then((fileData: FileSkelValues) => {
             console.log(fileData)
             resolve(fileData["values"]["downloadUrl"])
 
@@ -171,9 +195,9 @@ export class TextBone extends RawBone {
     for (const f of formats) {
       obj[f] = {inline: 'span', 'classes': `viur-txt-${f}`}
     }
-    const aligns = ["alignleft", "aligncenter","alignright"];
+    const aligns = ["alignleft", "aligncenter", "alignright"];
     for (const align of aligns) {
-      obj[align] = {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes: `viur-txt-${align}`}
+      obj[align] = {selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes: `viur-txt-${align}`}
     }
     console.log("obj", obj)
     return obj;
